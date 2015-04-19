5f07b0c4bec51d9660d1491bb8ac01f62913abd0
chrizchan <cha33315@gmail.com>
2015-04-19 14:40:39 +0800

    Controller

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
 (function(){
-  var eventsApp = angular.module("eventsApp",[""]);
+  var eventsApp = angular.module("eventsApp",[]);
 
 }());
--- app/controllers/EventController.js
+++ app/controllers/EventController.js
@@ -5,12 +5,18 @@
 'use strict';
 
 (function(){
-    var eventsApp = angular.module("eventsApp",[""]);
+    var eventsApp = angular.module("eventsApp",[]);
 
     var EventController = function($scope){
 
+        $scope.event = {
+            name: "Angular Boot Camp",
+            date: "1/1/2015",
+            time: "10:30 PM"
+        }
+
     };
 
-    eventsApp.controller('EventController', EventController);
+    eventsApp.controller("EventController", EventController);
 
 }());
